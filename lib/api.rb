3d62f7823ce673d6fb4ccbd7d7aaa02baa842454
mjones1818 <michael.jones0532@gmail.com>
2020-10-21 16:06:09 -0500

    working with all flights to and from airport

--- lib/api.rb
+++ lib/api.rb
@@ -18,14 +18,12 @@ class Api
       end
     end
     final.delete(nil)
+    make_flights(final.uniq)
     final.uniq
-    
-
   end
 
-  def make_flights(location)
-    all_flights = get_flights_overhead(location)
-    all_flights.each do |flight|
+  def make_flights(flights)
+    flights.each do |flight|
       if flight.match(/\D\d+\D+/)
         flight_number = flight
         airline = "Private "
@@ -65,38 +63,50 @@ class Api
     else
       airport = airport + "+airport"
     end
-    custom_url = "#{URL}#{APPID}&input=#{airport}&includepodid=FlightsBetweenSummary:To:FlightData&podstate=FlightsBetweenSummary:To:FlightData__More&output=json"
+    custom_url = "#{URL}#{APPID}&input=#{airport}&includepodid=FlightsBetweenSummary:To:FlightData&podstate=5@FlightsBetweenSummary:To:FlightData__More&output=json&Scantimeout=20&parsetimeout=20&formattimeout=20&podtimeout=20"
     uri = URI.parse(custom_url)
     response = Net::HTTP.get_response(uri)
     flights = JSON.parse(response.body)
-    binding.pry
+    #binding.pry
+    arrived_flights = flights["queryresult"]["pods"][0]["subpods"][0]["img"]["alt"].split("\n")
+    enroute_flights = flights["queryresult"]["pods"][0]["subpods"][1]["img"]["alt"].split("\n")
+    scheduled_flights = flights["queryresult"]["pods"][0]["subpods"][2]["img"]["alt"].split("\n")
+    arrived_flights_array = []
+    enroute_flights_array = []
+    scheduled_flights_array = []
+    arrived_flights.each do |data|
+      if data.include? "|"
+        split = data.split(" | ")
+        arrived_flights_array << split[0]
+      else
+        next
+      end
+    end
+
+    enroute_flights.each do |data|
+      if data.include? "|"
+        split = data.split(" | ")
+        enroute_flights_array << split[0]
+      else
+        next
+      end
+    end
+
+    scheduled_flights.each do |data|
+      if data.include? "|"
+        split = data.split(" | ")
+        arrived_flights_array << split[0]
+      else
+        next
+      end
+    end
+    self.make_flights(arrived_flights_array)
+    self.make_flights(enroute_flights_array)
+    self.make_flights(arrived_flights_array)
   end 
 end
 
-
-#http://api.wolframalpha.com/v2/query?appid=8P2YPV-XQVG699A8R&input=ord+airport&includepodid=FlightsBetweenSummary:To:FlightData&output=json
-# airline and flight number flights["queryresult"]["pods"][0]["subpods"][0]["img"]["alt"]
-# plain text route - flights["queryresult"]["pods"][1]["subpods"][0]["img"]["alt"]
-# dept airport - flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[0]
-# sched take off - flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[1]
-# arrival time - flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[2]
-# scheduled landing - flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[3]
-#estimates landing - flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[4]
-#estimated_flight_duration = flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[5]
-#time_since_takeoff = flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[6]
-
-
-#light_info_hash = {
-#     airline_and_flight_number:  flights["queryresult"]["pods"][0]["subpods"][0]["img"]["alt"],
-#     plain_text_route: flights["queryresult"]["pods"][1]["subpods"][0]["img"]["alt"],
-#     dept_airport: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[0],
-#     sched_take_off: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[1],
-#     arrival_time: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[2],
-#     scheduled_landing: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[3],
-#     estimated_landing: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[4],
-#     estimated_flight_duration: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[5],
-#     time_since_takeoff: flights["queryresult"]["pods"][3]["subpods"][0]["img"]["alt"].split("\n")[6]
-#   }
-
-
+# arrived_flights = flights["queryresult"]["pods"][0]["subpods"][0]["img"]["alt"]
+# enroute_flights = flights["queryresult"]["pods"][1]["subpods"][0]["img"]["alt"]
+# scheduled_flights = flights["queryresult"]["pods"][2]["subpods"][0]["img"]["alt"]
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -32,14 +32,74 @@ class Cli
         #scrape data
         Flights.reset_all
         first = Api.new
-        first.make_flights(input)
+        first.get_flights_overhead(input)
+        #first.make_flights(input)
         #show data (all flights or flights by airline)
         puts ''
         puts 'These are the flights currently over your location'
         Flights.all.each_with_index do |flight, index|
           puts "#{index+1}. #{flight.airline.name} flight #{flight.flight_number}"
         end
-        view_by_airline = Flights.all.count+1
+
+        show_flight_information
+        
+        #view_by_airline = Flights.all.count+1
+        #puts ''
+        #puts "#{view_by_airline}. VIEW BY AIRLINE"
+        #puts ""
+        ##ask for input
+        #puts "Select a flight for more information. Or type airline to see flights by airline"
+        #input = gets.chomp
+        ##second scrape
+        #if input.to_i.between?(1,Flights.all.count)
+        #  second = Api.new
+        #  selection_flight_number = Flights.all[input.to_i-1].flight_number
+        #  selection_airline = Flights.all[input.to_i-1].airline.name.split(" ").join("+")
+        #  second.flight_info(selection_flight_number,selection_airline)
+        #  #return results
+        #  Flights.print_information(selection_flight_number)
+        #elsif input == "airline" || input.to_i == view_by_airline
+        #  puts "Choose airline name"
+        #    Airlines.all.each_with_index do |airline, index|
+        #      puts "#{index+1}. #{airline.name}"
+        #    end
+        #  input = gets.chomp
+        #  matching_airline = Airlines.find_by_name(Airlines.all[input.to_i-1].name)
+        #  puts "#{matching_airline.name} Flights:"
+        #  flight_by_airline_array = []
+        #  matching_airline.flights.each_with_index do |flight, index| 
+        #    puts "#{index+1}. #{flight.flight_number}"
+        #    flight_by_airline_array << flight
+        #  end
+        #  puts "Select flight for more information"
+        #  input = gets.chomp
+        #  third = Api.new
+        #  selection_flight_number = flight_by_airline_array[input.to_i-1].flight_number
+        #  selection_airline = matching_airline.name
+        #  third.flight_info(selection_flight_number,selection_airline)
+        #  #return results
+        #  Flights.print_information(selection_flight_number)
+        #  
+        #end
+        when "2" 
+        puts ""
+        puts "Enter an airport code or city name"
+        puts ""
+        input = gets.chomp
+        Flights.reset_all
+        fourth = Api.new
+        fourth.airport_activity(input)
+        Flights.all.each_with_index do |flight, index|
+          puts "#{index+1}. #{flight.airline.name} flight #{flight.flight_number}"
+        end
+        show_flight_information
+      end
+    end
+  end
+
+
+  def show_flight_information
+    view_by_airline = Flights.all.count+1
         puts ''
         puts "#{view_by_airline}. VIEW BY AIRLINE"
         puts ""
@@ -77,16 +137,5 @@ class Cli
           Flights.print_information(selection_flight_number)
           
         end
-        when "2" 
-        puts ""
-        puts "Enter an airport code or city name"
-        puts ""
-        input = gets.chomp
-        Flights.reset_all
-        fourth = Api.new
-        fourth.airport_activity(input)
-      end
-    end
   end
-
 end
