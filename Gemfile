3c690cf6ec0fa2c173ef1559e6da08dea870ca8e
mjones1818 <michael.jones0532@gmail.com>
2020-10-24 18:29:13 -0500

    added gemfile and updated environment

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'pry', '~>0.13.1'
+gem 'open-uri', '~>0.1.0'
+gem 'json', '~>2.3'
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,13 @@
+require 'bundler/setup'
+Bundler.require(:default)
+
 require 'pry'
 require 'net/http'
 require 'open-uri'
 require 'json'
 
-require_relative 'cli.rb'
-require_relative 'api.rb'
-require_relative 'airlines.rb'
-require_relative 'flights.rb'
-require_relative 'airport.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/api.rb'
+require_relative '../lib/airlines.rb'
+require_relative '../lib/flights.rb'
+require_relative '../lib/airport.rb'
